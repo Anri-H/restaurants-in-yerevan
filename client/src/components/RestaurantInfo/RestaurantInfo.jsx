51f7bc3206e427cad321c6e4b7d6fc77d4c225d8
Anri <anrihunanyan@gmail.com>
2021-10-27 00:51:49 +0400

    refactoring(restaurantInfo.jsx): seperated comment section

--- client/src/components/RestaurantInfo/RestaurantInfo.jsx
+++ client/src/components/RestaurantInfo/RestaurantInfo.jsx
@@ -1,44 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { Rating } from "react-simple-star-rating";
-import { addComment } from "../../services/api";
+import Comment from "../Comment/Comment";
+import styles from "./restaurantInfo.module.css";
 
 export default function RestaurantInfo({ restaurant }) {
-  const [comment, setComment] = useState({ name: "", comment: "" });
   const { _id, name, description, rating, comments } = restaurant;
 
-  const handleChange = ({ target }) => {
-    setComment({ ...comment, [target.name]: target.value });
-  };
-
-  const handleSubmit = () => {
-    addComment(_id, comment);
-  };
-
   return (
-    <div key={_id}>
-      <h2>{name}</h2>
+    <div key={_id} className={styles.main}>
+      <h2 className={styles.title}>{name}</h2>
       <Rating ratingValue={rating} />
-      <p>{description}</p>
-      <div>
-        <h3>Comments</h3>
-        <input type="text" name="name" onChange={handleChange} />
-        <textarea
-          name="comment"
-          placeholder="Enter your comment"
-          id=""
-          cols="30"
-          rows="10"
-          onChange={handleChange}
-        ></textarea>
-        <button onClick={handleSubmit}>Submit</button>
-        {comments &&
-          comments.map(({ _id, name, comment }) => (
-            <div key={_id}>
-              <h3>{name}</h3>
-              <p>{comment}</p>
-            </div>
-          ))}
-      </div>
+      <p className={styles.description}>{description}</p>
+      <Comment comments={comments} id={_id} />
     </div>
   );
 }

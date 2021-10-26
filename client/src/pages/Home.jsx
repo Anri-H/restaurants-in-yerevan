eac37836885a35a95e7988ea2cdd0e5a857e5918
Anri <anrihunanyan@gmail.com>
2021-10-26 23:27:06 +0400

    refactoring(Home.jsx): styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getRestaurants } from "../services/api";
 import Map from "../components/Map/Map";
 import RestaurantsCard from "../components/RestaurantsCard/RestaurantsCard";
+import styles from "./home.module.css";
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -15,15 +16,19 @@ export default function Home() {
 
   return (
     <div>
+      <h1>Top 5 Restaurants in Yerevan</h1>
+
       {data && (
-        <>
-          {data.map((restaurant, i) => (
-            <div key={i} onClick={() => setMapData(restaurant)}>
-              <RestaurantsCard restaurant={restaurant} />
-            </div>
-          ))}
+        <div className={styles.main}>
+          <div className={styles.list}>
+            {data.map((restaurant, i) => (
+              <div key={i} onClick={() => setMapData(restaurant)}>
+                <RestaurantsCard restaurant={restaurant} />
+              </div>
+            ))}
+          </div>
           <Map data={mapData ? mapData : data} />
-        </>
+        </div>
       )}
     </div>
   );

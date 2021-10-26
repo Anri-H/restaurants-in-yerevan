d14d495fc58188d7b42a5519845ccc4089742dc8
Anri <anrihunanyan@gmail.com>
2021-10-26 23:28:04 +0400

    refactoring(restaurantsCard): styles structure

--- client/src/components/RestaurantsCard/RestaurantsCard.jsx
+++ client/src/components/RestaurantsCard/RestaurantsCard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Rating } from "react-simple-star-rating";
+import styles from "./restaurantsCard.module.css";
 
 export default function RestaurantsCard({ restaurant }) {
   const { _id, name, description, rating } = restaurant;
 
   return (
-    <div key={_id}>
+    <div key={_id} className={styles.card}>
       <h2>{name}</h2>
       <Rating ratingValue={rating} />
       <p>{description}</p>

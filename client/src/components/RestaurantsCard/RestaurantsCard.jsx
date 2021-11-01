cdd5f63397edf626f5835f0c3f6b7583f629ab4f
Anri <anrihunanyan@gmail.com>
2021-11-01 22:36:40 +0400

    feat(RestaurantsCard): added img in card & corrected structure

--- client/src/components/RestaurantsCard/RestaurantsCard.jsx
+++ client/src/components/RestaurantsCard/RestaurantsCard.jsx
@@ -4,16 +4,18 @@ import { Rating } from "react-simple-star-rating";
 import styles from "./restaurantsCard.module.css";
 
 export default function RestaurantsCard({ restaurant }) {
-  const { _id, name, description, rating } = restaurant;
+  const { _id, name, rating, image } = restaurant;
 
   return (
     <div key={_id} className={styles.card}>
-      <h3>{name}</h3>
-      <Rating ratingValue={rating} />
-      <p className={styles.description}>{description}</p>
-      <Link to={`/${_id}`} className={styles.link}>
-        View Restaurant
-      </Link>
+      <img src={image} width="100%" alt="name" />
+      <div className={styles.info}>
+        <h5>{name}</h5>
+        <Rating ratingValue={rating} />
+        <Link to={`/${_id}`} className={styles.link}>
+          View Restaurant
+        </Link>
+      </div>
     </div>
   );
 }

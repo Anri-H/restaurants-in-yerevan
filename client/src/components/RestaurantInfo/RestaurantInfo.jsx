86cc6c9c45dc57eaa128067cc4ab0c0396c56c28
Anri <anrihunanyan@gmail.com>
2021-11-01 22:35:25 +0400

    fix(RestaurantInfo): corrected structure & styles

--- client/src/components/RestaurantInfo/RestaurantInfo.jsx
+++ client/src/components/RestaurantInfo/RestaurantInfo.jsx
@@ -4,13 +4,20 @@ import Comment from "../Comment/Comment";
 import styles from "./restaurantInfo.module.css";
 
 export default function RestaurantInfo({ restaurant }) {
-  const { _id, name, description, rating, comments } = restaurant;
+  const { _id, name, description, rating, image, geometry, comments } =
+    restaurant;
 
   return (
     <div key={_id} className={styles.main}>
-      <h2 className={styles.title}>{name}</h2>
-      <Rating ratingValue={rating} />
-      <p className={styles.description}>{description}</p>
+      <div className={styles.info}>
+        <img src={image} width="50%" alt={name} />
+        <div>
+          <h2 className={styles.title}>{name}</h2>
+          <Rating ratingValue={rating} />
+          <p>{geometry.address}</p>
+          <p className={styles.description}>{description}</p>
+        </div>
+      </div>
       <Comment comments={comments} id={_id} />
     </div>
   );
